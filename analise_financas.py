941f30a68c23fbc3e65e443f2241e381a6922cd3
pedro.minella1 <pedro.minella1@gmail.com>
2026-03-26 08:56:59 -0300

    atualizacao

--- analise_financas.py
+++ analise_financas.py
@@ -2,19 +2,29 @@ import requests
 import pandas as pd
 
 base_url = "https://laboratoriodefinancas.com/api/v2"
-token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNzc2OTQyNzc3LCJpYXQiOjE3NzQzNTA3NzcsImp0aSI6IjhhOTEzODYwYjM1NTQ4ZjNiNTViNGY3ODgyYzYyMzk2IiwidXNlcl9pZCI6Ijk1In0.PvyR5WkC5FORHaSx4d_7Bsgo0w_nzKlKTAjcUH4wz68"
+token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNzc3MTE3NTU1LCJpYXQiOjE3NzQ1MjU1NTUsImp0aSI6IjcwMDA4ODAxNzI0NjQyNGU4Y2FkODE4OTUwZjYzY2E2IiwidXNlcl9pZCI6Ijk1In0.XZ0cOvobMFDAch9x0Q0lCOoTonMGw-Rn2scTLq800i0"
+
 resp = requests.get(
-    f"{base_url}/bolsa/preco-planilhao",
+    f"{base_url}/bolsa/planilhao",
     headers={"Authorization": f"Bearer {token}"},
-    params={"data_base": "2026-03-23"},
+    params={"data_base": "2025-03-21"},
 )
 
-print(resp.status_code)
+print("status:", resp.status_code)
+print("content-type:", resp.headers.get("Content-Type"))
+print("texto da resposta:")
 print(resp.text)
 
 # Filtrar empresa com maior ROE
 dados = resp.json()
 df = pd.DataFrame(dados)
+df2 = df[["ticker", "roe", "p_vp"]]
+df2["rank_roe"] = df2["roe"].rank()
+df2["rank_p_vp"] = df2["p_vp"].rank(ascending=True)
+df2["rank_final"] = (df2["rank_roe"] + df2["rank_p_vp"]) / 2
+df2.sort_values("rank_final", ascending=False)
+
+
 maximo = df["roe"].max()
 filtro = df["roe"]==maximo
 df[filtro]
@@ -24,7 +34,7 @@ import requests
 
 base_url = "https://laboratoriodefinancas.com/api/v2"
 token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNzc2OTQyNzc3LCJpYXQiOjE3NzQzNTA3NzcsImp0aSI6IjhhOTEzODYwYjM1NTQ4ZjNiNTViNGY3ODgyYzYyMzk2IiwidXNlcl9pZCI6Ijk1In0.PvyR5WkC5FORHaSx4d_7Bsgo0w_nzKlKTAjcUH4wz68"
-params = {"ticker": "MNPR3", "data_ini": "2025-03-21", "data_fim": "2026-03-23"}
+params = {"ticker": "BBSE3", "data_ini": "2025-03-21", "data_fim": "2026-03-23"}
 resp = requests.get(
     f"{base_url}/preco/corrigido",
     headers={"Authorization": f"Bearer {token}"},
@@ -47,6 +57,38 @@ preco_final / preco_inicial -1
 
 
 # API para pegar do IBOV
+
+import yfinance as yf
+# Get a ticker data
+# teste back 1 ano
+ibov = yf.download("^BVSP", start="2025-03-21", end="2026-03-26")
+ibov.columns
+# Esta como index a data dai vc faz ibov.index
+filtro_1 = ibov.index == "2025-03-21"
+preco_inicial_ibov = ibov[filtro_1]["Close"].iloc[0]
+
+filtro_2 = ibov.index == "2026-03-23"
+preco_final_ibov = ibov[filtro_2]["Close"].iloc[0]
+print(preco_final_ibov)
+preco_final_ibov / preco_inicial_ibov -1
+
+import yfinance as yf
+# Get a ticker data
+# teste back 5 ano
+ibov = yf.download("^BVSP", start="2021-03-21", end="2026-03-26")
+ibov.columns
+# Esta como index a data dai vc faz ibov.index
+filtro_1 = ibov.index == "2021-03-25"
+preco_inicial_ibov = ibov[filtro_1]["Close"].iloc[0]
+
+filtro_2 = ibov.index == "2026-03-23"
+preco_final_ibov = ibov[filtro_2]["Close"].iloc[0]
+print(preco_final_ibov)
+preco_final_ibov / preco_inicial_ibov -1
+
+
+
+
 
 base_url = "https://laboratoriodefinancas.com/api/v2"
 token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNzc2OTQyNzc3LCJpYXQiOjE3NzQzNTA3NzcsImp0aSI6IjhhOTEzODYwYjM1NTQ4ZjNiNTViNGY3ODgyYzYyMzk2IiwidXNlcl9pZCI6Ijk1In0.PvyR5WkC5FORHaSx4d_7Bsgo0w_nzKlKTAjcUH4wz68"
